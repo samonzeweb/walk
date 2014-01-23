172a799dcf2dc0c801b2cc9769c3287ed555683a
samonzeweb <sam.onzeweb@gmail.com>
2014-01-23 20:07:38 +0100

    Add specs to do

--- spec/walk_spec.rb
+++ spec/walk_spec.rb
@@ -7,10 +7,14 @@ describe Walk do
 
   it 'should find all directories in directory tree'
 
+  it 'should return an enumerator if no block is passed'
+
   it 'should return content from top to bottom by default'
 
   it 'should not explore symlink by default'
 
   it 'should explore symlink if needed'
+
+  it 'should ignore disapearing entries'
 
 end
